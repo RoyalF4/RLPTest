2d82cd85bf3eba6780530dbbfe415fbbe1e0a113
RoyalF4 <Royal2791@gmail.com>
2021-02-16 12:02:44 -0500

    Uploading Project To Repository

---
+++ app/src/main/java/android/example/rlptest/LoginSuccess.java
@@ -0,0 +1,14 @@
+package android.example.rlptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginSuccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_success);
+    }
+}
